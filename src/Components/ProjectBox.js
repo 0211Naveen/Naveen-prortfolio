c99aa964c4c03f3c08b230724d5e37af844d7eb5
Naveen <naveen0211@gmail.com>
2025-11-13 14:32:14 +0530

    antique website

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -5,21 +5,30 @@ import {CgFileDocument} from "react-icons/cg";
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub : "https://github.com/DevanshSahni/tindog",
-    TindogWebsite : "https://devanshsahni.github.io/tindog/",
 
-    RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
-    RogFreeGithub : "https://github.com/DevanshSahni/Rog-Free",
-    RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
 
-    NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
-    NewsletterGithub:"",
-    NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
+    // TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
+    // TindogGithub : "https://github.com/DevanshSahni/tindog",
+    // TindogWebsite : "https://devanshsahni.github.io/tindog/",
+
+    // RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
+    // RogFreeGithub : "https://github.com/DevanshSahni/Rog-Free",
+    // RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
+
+    // NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
+    // NewsletterGithub:"",
+    // NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
     
-    WigglesDesc:"An innovative pet management web app enabling pet parents to create unique pet IDs, securely store and share vaccination records, and generate QR codes for pet profiles, enhancing safety.",
-    WigglesGithub:"https://github.com/DevanshSahni/Wiggles",
-    WigglesWebsite:"https://wiggles.vercel.app/",
+    AntiqueDesc:"Built a simple Antique Product Selling Website with an Admin Panel for managing products and orders.",
+    AntiqueGithub:"https://github.com/0211Naveen/E-com.git",
+    AntiqueWebsite:"https://e-com-olive-rho.vercel.app/",
+
+
+    MessageDesc:"Message App is a full-stack MERN (MongoDB, Express.js, React.js, Node.js) application that allows users to send and store messages in real time.It demonstrates secure backend integration with MongoDB Atlas, smooth frontend-to-backend communication using REST APIs, and modern deployment workflows using Vercel.",
+    MessageGithub:"https://github.com/0211Naveen/message-app.git",
+    MessageWebsite:"https://message-app-client-wine.vercel.app/",
+
+
   }
 
   let show ='';
